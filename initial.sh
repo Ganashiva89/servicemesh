eb1b6599ff51bfba0fb00d53395b5b08391491a0
Ganashiva89 <36024776+Ganashiva89@users.noreply.github.com>
2024-04-15 19:06:48 +0530

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -10,18 +10,31 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https gnupg2
 
 # Add the Kubernetes repository key and repository
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
+# If the directory `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Update the package list again to get the new repositories
 sudo apt-get update
 
 # Install Docker and Kubernetes
 #sudo apt-get install -y docker-ce kubelet=1.15.7-00 kubeadm=1.15.7-00 kubectl=1.15.7-00
-sudo apt-get install -y kubectl kubeadm kubelet kubernetes-cni docker.io
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
 sudo systemctl start docker
 sudo systemctl enable docker
+
+sudo systemctl enable --now kubelet
 
 #Add the current user to the Docker group
 sudo usermod -aG docker $USER
